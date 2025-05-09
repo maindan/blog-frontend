075fa97e486d617dc0688585bb50c5c4293c7856
Daniel <danielima.projex@gmail.com>
2025-05-09 14:52:36 -0400

    feat: add footer & home adjustments

--- src/app/core/home/home.component.ts
+++ src/app/core/home/home.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "../header/header.component";
+import { FooterComponent } from "../footer/footer.component";
 
 @Component({
   selector: 'app-home',
   imports: [
     RouterOutlet,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
 ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'

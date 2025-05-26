91b149a4ffada560ef0c3efe093bd0ab9eb225ed
Daniel <danielima.projex@gmail.com>
2025-05-26 12:57:37 -0400

    feat: change created by field in interface

--- src/app/shared/interfaces/post.ts
+++ src/app/shared/interfaces/post.ts
@@ -1,15 +1,16 @@
 export interface IPostCreate {
-  title: string,
-  subtitle: string,
-  content: string,
-  created_by: number,
-  tags?: ITag[]
+  title: string;
+  subtitle: string;
+  content: string;
+  tagstag_ids?: number[];
 }
 
 export interface IPost extends IPostCreate {
   id: number,
+  created_by: string,
   created_at: string,
-  updated_at: string
+  updated_at: string,
+  tags: ITag[]
 }
 
 export interface ITag {
